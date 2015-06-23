7da62b716829a4cc27f3dbf4723be914827ae953
ali <saabdoli@gmail.com>
2015-06-23 10:04:15 -0500

    Switched to object-oriented approach

--- editor.py
+++ editor.py
@@ -1,12 +1,19 @@
 from Tkinter import *
 import ScrolledText
 
-# Initialize root window
-root = Tk()
 
-# Create scrollbar
-txt_area = ScrolledText.ScrolledText(root, width=100, height=80)
+class MainApp(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
 
-txt_area.pack()
+        # Create scrollbar
+        txt_area = ScrolledText.ScrolledText(root, width=100, height=80)
+        txt_area.pack()
 
-root.mainloop()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    MainApp(root).pack(side='top', fill='both', expand=True)
+    root.mainloop()
